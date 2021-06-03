1eba0df682e296c1a34be6aa263c7b0f087481a5
shanna <shanna615615615.sy@gmail.com>
2021-06-03 21:37:05 +0800

    登入頁API

---
+++ SocialNetworkTest2021/Controllers/API/MemberAPIController.cs
@@ -0,0 +1,21 @@
+﻿using SocialNetworkTest2021.Models;
+using System.Web.Http;
+
+namespace SocialNetworkTest2021.Controllers.API
+{
+    [RoutePrefix("API/MemberAPI")]
+    public class MemberAPIController : ApiController
+    {
+        [HttpPost, Route(nameof(MemberAPIController.Login))]
+        public ResponseViewModel<string> Login(LoginViewModel loginViewModel)
+        {
+            string response = "帳號:" + loginViewModel.Account + "\n密碼:" + loginViewModel.Password;
+            ResponseViewModel<string> result = new ResponseViewModel<string>
+            {
+                ResultCode = 1,
+                Message = response,
+            };
+            return result;
+        }
+    }
+}
---
+++ SocialNetworkTest2021/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SocialNetworkTest2021.Models
+{
+    public class LoginViewModel
+    {
+        public string Account { get; set; }
+        public string Password { get; set; }
+    }
+}
