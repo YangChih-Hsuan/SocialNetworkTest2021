fd0e16c8e7e5ef3df25c27b66319b13d949d0860
shanna <shanna615615615.sy@gmail.com>
2021-06-17 21:51:41 +0800

    SingupPOPTest

--- SocialNetworkTest2021/ViewModel/LoginViewModel.cs
+++ SocialNetworkTest2021/ViewModel/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿namespace SocialNetworkTest2021.ViewModel
 {
+    //登入
     public class LoginViewModel
     {
         public string Account { get; set; }
--- SocialNetworkTest2021/ViewModel/SingupViewModel.cs
+++ SocialNetworkTest2021/ViewModel/SingupViewModel.cs
@@ -1,7 +1,10 @@
 ﻿namespace SocialNetworkTest2021.ViewModel
 {
+    //註冊
     public class SingupViewModel
     {
-
+        public string Account { get; set; }
+        public string Password { get; set; }
+        //public static Mail {get; set;}
     }
 }
