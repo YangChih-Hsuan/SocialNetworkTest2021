b3c1bac6450a3163071feb1a22aacab479d0e44b
shanna <shanna615615615.sy@gmail.com>
2021-07-18 23:34:37 +0800

    首頁UI版面寫法調整、會員登入顯示

--- SocialNetworkTest2021/Controllers/HomeController.cs
+++ SocialNetworkTest2021/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using SocialNetworkTest2021.Model;
+using SocialNetworkTest2021.ViewModel;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace SocialNetworkTest2021.Controllers
 {
@@ -6,13 +9,23 @@ namespace SocialNetworkTest2021.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var memberdb = new SocialNetworkTest2021Entities();
+            //Leo換成代入index方法的值，來源是從登入時來
+            var memberData = memberdb.Member.Where(w => w.Account == "Leo").Select(s => new IndexViewModel()
+            {
+                MemberID = s.MemberID,
+                NickName = s.NickName
+            }).FirstOrDefault();
+
+            TempData["Message"] = "TempData測試資料";
+            return View(memberData);
         }
 
-        public ActionResult About()
+        public ActionResult About(int memberID)
         {
-            ViewBag.Message = "Your application description page.";
-
+            //about取得會員ID
+            ViewBag.Message = $"Your memberID is {memberID}.";
+           
             return View();
         }
 
---
+++ SocialNetworkTest2021/ViewModel/IndexViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace SocialNetworkTest2021.ViewModel
+{
+    /// <summary>
+    /// Index ViewModel
+    /// </summary>
+    public class IndexViewModel
+    {
+        /// <summary>
+        /// 會員編號
+        /// </summary>
+        public int MemberID { get; set; }
+
+        /// <summary>
+        /// 暱稱
+        /// </summary>
+        public string NickName { get; set; }
+    }
+}
