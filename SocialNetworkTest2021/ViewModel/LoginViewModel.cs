ff29bf97d2123654fc398a4459f0e5fe86ccba1b
shanna <shanna615615615.sy@gmail.com>
2021-08-31 21:23:36 +0800

    MemberInfo POP UI

--- SocialNetworkTest2021/ViewModel/LoginViewModel.cs
+++ SocialNetworkTest2021/ViewModel/LoginViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace SocialNetworkTest2021.ViewModel
+﻿using System;
+
+namespace SocialNetworkTest2021.ViewModel
 {
     //登入
     public class LoginViewModel
@@ -6,4 +8,11 @@
         public string Account { get; set; }
         public string Password { get; set; }
     }
+    //測試
+    public class DateViewModel
+    {
+        public DateTime Brithdate { get; set; }
+
+    }
+
 }
